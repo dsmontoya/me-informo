64304540efe1c2b332dd56ab37c0ca597ab22170
Daniel Montoya <dsmontoyam@gmail.com>
2016-06-11 12:16:41 -0500

    Nuevo proyecto de Rails.

---
+++ config/locales/es.rb
@@ -0,0 +1,15 @@
+{
+  es: {
+    title: "Plataforma nacional de Transparencia",
+    application: {
+      index: {
+        title: "¡Bienvenido a la Plataforma Nacional de Transparencia!",
+        subtitle: "Con esta plataforma podrás solicitar de manera sencilla información pública de tu interés."
+      },
+      information_granted: {
+        title: "Búsqueda de Sujetos Obligados",
+        subtitle: "Cualquier figura que reciba y ejerza recursos públicos o realice actos de autoridad en el ámbito federal, estatal y municipal"
+      }
+    }
+  }
+}
